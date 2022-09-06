5b25fc80ce4def7dd621318c032aa8efc8b00cf3
Alisa <petrunina.alisa@gmail.com>
2022-09-06 22:42:14 +0300

    Task1

---
+++ HW_task1.py
@@ -0,0 +1,13 @@
+# Задача 1. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+# Пример:
+# - 6 -> да
+# - 7 -> да
+# - 1 -> нет
+
+# day = int(input('Введите число: '))
+# if day < 1 or day > 7:
+#     print('It is an input mistake')
+# elif day ==6 or day==7:
+#     print('Greate! It is weekend!')
+# else:
+#     print('No, it is not weekend')
