a690fc4485025ef26e51ab23327af9c6c8e63e8e
Alisa <petrunina.alisa@gmail.com>
2022-09-06 22:43:18 +0300

    HW

--- HomeWork.py
+++ HomeWork.py
@@ -25,6 +25,7 @@
 # for x in range(2):
 #         for y in range(2):
 #             for z in range(2):
+#                 print(f'x={x}, y={y}, z={z}')
 #                 print(not (x or y or z) == (not x and not y and not z))
 
 
